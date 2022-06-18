a0f31d1b0b7c3400adac1ff0b03d95c918adc29a
samithperera94 <samiththilanka94@gmail.com>
2022-06-18 23:56:12 +0530

    use styled component to add css dynamically

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,12 +10,15 @@ const FormControl = styled.div`
       font-weight: bold;
       display: block;
       margin-bottom: 0.5rem;
+      color : ${props => props.invalid ? 'red' : 'black'}
     }
 
     & input {
       display: block;
       width: 100%;
-      border: 1px solid #ccc;
+      // border: 1px solid #ccc;
+      border: 1px solid ${props => props.invalid ? 'red' : '#ccc'};
+      background:${props => props.invalid ? '#ffd7d7' : 'transparent'} ;
       font: inherit;
       line-height: 1.5rem;
       padding: 0 0.25rem;
@@ -26,13 +29,13 @@ const FormControl = styled.div`
       background: #fad0ec;
       border-color: #8b005d;
     }
-    &.invalid input{
-      border-color: red;
-      background: #ffd7d7;
-    }
-    &.invalid label{
-      border-color: red;
-    }
+    // &.invalid input{
+    //   border-color: red;
+    //   background: #ffd7d7;
+    // }
+    // &.invalid label{
+    //   border-color: red;
+    // }
 
 `;
 
@@ -69,7 +72,8 @@ const CourseInput = props => {
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div> */}
-      <FormControl className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      {/* <FormControl className={`${!isValid && 'invalid'}`}> */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
